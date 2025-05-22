e033892c5450a392bf362935bb81c82a8882fba8
Yonnie <eduardo.gailoti@gmail.com>
2025-05-21 23:50:42 -0300

    primeiro

---
+++ Calculadora.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Calculadora {
+    class Calculadora {
+        public double PrimeiroNumero;
+        public double SegundoNumero;
+        public string Operacao;
+
+        public Calculadora(double primeiroNumero, double segundoNumero, string operacao) {
+            PrimeiroNumero = primeiroNumero;
+            SegundoNumero = segundoNumero;
+            Operacao = operacao;
+        }
+
+        public double Somar(double primeiro, double segundo) {
+            double resultado = primeiro + segundo;
+            return resultado;
+        }
+
+        public double Subtracao(double primeiro, double segundo) {
+            double resultado = primeiro - segundo;
+            return resultado;
+        }
+
+        public double Multiplicacao(double primeiro, double segundo) {
+            double resultado = primeiro * segundo;
+            return resultado;
+        }
+
+        public double Divisao(double primeiro, double segundo) {
+            double resultado;
+
+            while (segundo <= 0) {
+                Console.Write("Divisão inválida, tente divisor acima de 0: ");                
+            }
+
+            resultado = primeiro / segundo;
+            return resultado;
+        }
+
+        public double Escolha(string operacao, double primeiro, double segundo) {
+            switch (operacao) {
+                case "+":
+                    return Somar(primeiro, segundo);                   
+                case "-":
+                    return Subtracao(primeiro, segundo);                    
+                case "*":
+                    return Multiplicacao(primeiro, segundo);                    
+                case "/":
+                    return Divisao(primeiro, segundo);                    
+                default:
+                    Console.WriteLine("Operação inválida!");                   
+                    return double.NaN;
+            }            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Calculadora {
+    class Program {
+        static void Main(string[] args) {
+            var culture = CultureInfo.InvariantCulture;
+
+            
+            Console.Write("Digite o primeiro número: ");
+            double primeiroNumero = double.Parse(Console.ReadLine(), culture);
+            Console.Write("Digite o segundo número: ");
+            double segundoNumero = double.Parse(Console.ReadLine(), culture);
+            Console.Write("Operação: ");
+            string operacao = Console.ReadLine();
+
+            Calculadora calc = new Calculadora(primeiroNumero, segundoNumero, operacao);
+
+            Console.WriteLine($"Resultado da operação: {calc.Escolha(operacao, primeiroNumero, segundoNumero)}");
+
+        }
+    }
+}
